ee96a47e296b62345c1606876db015ac3a87ec77
OriginalHades <myemail>
2025-03-02 08:44:49 +0100

    Structures now dont get generated with intersections.

--- include/game/world/region_lookup.hpp
+++ include/game/world/region_lookup.hpp
@@ -30,7 +30,9 @@ class RegionRegistry{
         std::unordered_map<glm::ivec3, std::vector<std::shared_ptr<Region>>, IVec3Hash, IVec3Equal> regions;
 
     public:
-        void add(const glm::ivec3& position, const glm::ivec3& size, const T& value){
+        bool add(const glm::ivec3& position, const glm::ivec3& size, const T& value){
+            if(instersects(position, size)) return false;
+
             glm::ivec3 min = position;
             glm::ivec3 max = position + size;
 
@@ -58,7 +60,48 @@ class RegionRegistry{
 
                 regions[subregion_position].push_back(region);
             }
+
+            return true;
         }
+        bool instersects(const glm::ivec3& position, const glm::ivec3& size){
+            glm::ivec3 min = position;
+            glm::ivec3 max = position + size;
+
+            std::array<glm::vec3, 8> points = {
+                min,
+                {min.x,max.y,min.z},
+                {min.x,max.y,max.z},
+                {min.x,min.y,max.z},
+                {max.x,min.y,min.z},
+                {max.x,max.y,min.z},
+                max,
+                {max.x,min.y,max.z}
+            };
+
+            std::unordered_set<glm::ivec3, IVec3Hash, IVec3Equal> already_checked;
+            std::shared_lock lock(mutex);
+
+            for(auto& point: points){
+                glm::ivec3 subregion_position = glm::floor(glm::vec3(point) / subregion_size);
+                
+                if(already_checked.contains(subregion_position)) continue;
+                already_checked.emplace(subregion_position);
+                if(!regions.contains(subregion_position)) continue;
+
+                for(auto& region: regions.at(subregion_position)){
+                    if(
+                        max.x >= region->min.x && min.x < region->max.x &&
+                        max.y >= region->min.y && min.y < region->max.y &&
+                        max.z >= region->min.z && min.z < region->max.z
+                    ){
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        
 
         Region* get(const glm::ivec3& position){
             glm::ivec3 subregion_position = glm::floor(glm::vec3(position) / subregion_size);
